349bdec50ba1795e191c1fc17467e893d64ca960
bulldra <bullda@gmail.com>
2021-05-01 23:41:17 +0900

    月替わりセール取得

--- src/main.py
+++ src/main.py
@@ -19,13 +19,27 @@ class Main:
     def main(self, args):
         self.logger.info(args)
         amazon_api = AmazonAPI(settings.amazon_access_key, settings.amazon_secret_key, settings.amazon_assosiate_id, 'JP')
-        products = amazon_api.search_products(item_count=25, keywords='python')
-        for x in products:
-            print(f'{x.title}\t{x.url}')
+        with open(settings.outfile, 'w', encoding='utf-8') as out:
+            out.write('title,price,url\n')
+            result = []
+            now_price = 1
+            for i in range(0, 2):
+                min_price = now_price * 100
+                for i in range(1, 11):
+                    self.logger.info(f"browse_node='3550442051',item_count=10, item_page={i}, min_price={min_price}")
+                    products = amazon_api.search_products(browse_node='3550442051',item_count=10, item_page=i, items_per_page=10, min_price=min_price, sort_by='Price:LowToHigh')
+                    if products is None:
+                        break
+                    for x in products:
+                        if len(list(filter(lambda y: y.title == x.title, result))) == 0:
+                            self.logger.info(f'{x.title}')
+                            result.append(x)
+                        now_price = x.prices.price.value
+            for x in sorted(result, key=lambda p : p.title):
+                out.write(f'{x.title},{x.prices.price.value},{x.url}\n')
 
 if(__name__ == '__main__'):
     parser = argparse.ArgumentParser()
     parser.add_argument("--version", action="version", version="%(prog)s (version {version})".format(version=__version__))
-#    parser.add_argument("args1")
     args = parser.parse_args()
     Main().main(args)
--- src/settings.py
+++ src/settings.py
@@ -6,12 +6,13 @@ import json
 
 def load_json_setting(path):
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    conf = open(path, 'r')
-    return json.load(conf)
+    with open(path, 'r') as conf:
+        return json.load(conf)
 
 settings_dict = load_json_setting('../config/settings.json')
 secrets_dict = load_json_setting('../config/secrets.json')
 
+outfile = settings_dict['outfile']
 amazon_access_key = secrets_dict['amazon_access_key']
 amazon_secret_key = secrets_dict['amazon_secret_key']
 amazon_assosiate_id = secrets_dict['amazon_assosiate_id']

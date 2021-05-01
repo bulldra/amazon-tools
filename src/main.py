b72ed78b5f9d74ededf9f1ce8790ca673aa17809
bulldra <bullda@gmail.com>
2021-05-02 07:54:38 +0900

    フィルタールールのモジュール化

--- src/main.py
+++ src/main.py
@@ -21,28 +21,26 @@ class Main:
     def main(self, args):
         self.logger.info(args)
         amazon_api = AmazonAPI(settings.amazon_access_key, settings.amazon_secret_key, settings.amazon_assosiate_id, 'JP')
+        result = set()
+        now_price = 1
+        for i in range(0, 3):
+            min_price = now_price * 100
+            for i in range(1, 11):
+                self.logger.info(f"request browse_node='{settings.monthly_node}', item_count=10, item_page={i}, min_price={min_price}, sort_by='Price:LowToHigh'")
+                products = amazon_api.search_products(browse_node=settings.monthly_node,item_count=10, item_page=i, items_per_page=10, min_price=min_price, sort_by='Price:LowToHigh')
+                if products is None:
+                    self.logger.info(f'response {products}')
+                    break
+                self.logger.info(f'response {len(products)} items')
+                for x in products:
+                    result.add(ProductWrapper(x))
+                    now_price = x.prices.price.value
+            else:
+                continue
+            break
+
         with open(settings.outfile, 'w', encoding='utf-8') as out:
             out.write('author,title,price,url\n')
-            result = set()
-            now_price = 1
-            for i in range(0, 3):
-                min_price = now_price * 100
-                for i in range(1, 11):
-                    self.logger.info(f"request browse_node='{settings.monthly_node}', item_count=10, item_page={i}, min_price={min_price}, sort_by='Price:LowToHigh'")
-                    products = amazon_api.search_products(browse_node=settings.monthly_node,item_count=10, item_page=i, items_per_page=10, min_price=min_price, sort_by='Price:LowToHigh')
-                    if products is None:
-                        self.logger.info(f'response {products}')
-                        break
-
-                    self.logger.info(f'response {len(products)} items')
-                    for x in products:
-                        print(p.info.product_group)
-                        p = ProductWrapper(x)
-                        result.add(p)
-                        now_price = p.price_value
-                else:
-                    continue
-                break
 
             for x in filter(lambda x : product_filter_rule.is_need(x, settings), sorted(result)):
                 out.write(f'"{x.author}","{x.title}","{x.price_display}","{x.url}"\n')

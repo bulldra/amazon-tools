ea7f835bcbf4ef5fd6ea16ff76e00347d48fc516
bulldra <bullda@gmail.com>
2021-05-04 10:42:47 +0900

    書籍名正規化処理

--- src/kindle_util.py
+++ src/kindle_util.py
@@ -1,30 +1,30 @@
 import re
 
 def tlanslate_series_title(title):
-    title = first_remove(title)
-    title = second_remove(title)
+    title = all_remove(title)
+    title = choice_remove(title)
     return title
 
-def first_remove(title):
-    title = re.sub('\\s*[\\[【〔].+?[\\]】〕]\\s*','',title)
-    title = re.sub('\\s*(完全|通常)版\\s*','',title)
+def all_remove(title):
+    regex_list =[
+        '\\s*[\\[【〔].+?[\\]】〕]\\s*',
+        '\\s*(完全|通常)版\\s*'
+    ]
+
+    for r in regex_list:
+        title = re.sub(r,'',title)
     return title
 
-def second_remove(title):
-    result = re.sub('([\\s0-9０-９]+年)*\\s*[0-9０-９]+月[号\\s]*','',title)
-    if title != result:
-        return result
+def choice_remove(title):
+    regex_list =[
+        '([\\s0-9０-９]+年)*\\s*[0-9０-９]+月[号\\s]*',
+        '[\\s＜（\\(第：]+([VvＶｖ][OoＯｏ][LlＬｌ．][\\.．])*([NnＮn][OoＯｏ][\\.．])*[0-9０-９一二三四五六七八九十IⅤＩX上中下]+[巻\\s\\)）＞]*',
+        '[第：]*[0-9０-９一二三四五六七八九IⅤＩX]+[巻\\s]+',
+        '[第：]*[0-9０-９一二三四五六七八九IⅤＩX]+[巻\\s]*$'
+    ]
 
-    result = re.sub('[\\s＜（\\(第：]+([VvＶｖ][OoＯｏ][LlＬｌ．][\\.．])*([NnＮn][OoＯｏ][\\.．])*[0-9０-９一二三四五六七八九十IⅤＩX上中下]+[巻\\s\\)）＞]*', '', title)
-    if title != result:
-        return result
-
-    result = re.sub('[第：]*[0-9０-９一二三四五六七八九IⅤＩX]+[巻\\s]+','',title)
-    if title != result:
-        return result
-
-    result = re.sub('[第：]*[0-9０-９一二三四五六七八九IⅤＩX]+[巻\\s]*$','',title)
-    if title != result:
-        return result
-
-    return result
+    for r in regex_list:
+        result = re.sub(r,'',title)
+        if title != result:
+            return result
+    return title

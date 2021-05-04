e7adb02b3918443bdd5a183778941f84b8faea15
bulldra <bullda@gmail.com>
2021-05-05 08:29:11 +0900

    削除規定の明確化

--- src/kindle_util.py
+++ src/kindle_util.py
@@ -6,9 +6,10 @@ def tlanslate_series_title(title):
     return title
 
 def all_remove(title):
+
     regex_list =[
-        '\\s*[\\[【〔].+?[\\]】〕]\\s*',
-        '\\s*(完全|通常|モノクロ|カラー)版\\s*'
+        '\\s*[\\[〔【\\(]*(雑誌|部分販売|マップ編|極！合本シリーズ|増補改訂版|合本版|書き下ろしイラスト付|完全版|通常版|モノクロ版|カラー版|Amazon.co.jp限定描き下ろし特典付)[】〕\\]\\)]*\\s*',
+        '（完）'
     ]
 
     for r in regex_list:
@@ -27,7 +28,6 @@ def choice_remove(title):
         '\\s*―*[第全]*[0-9０-９一二三四五六七八九十IⅤＩX]+巻―*\\s*',
         '\\s*[0-9０-９一二三四五六七八九IⅤＩX]+$',
     ]
-
 
     for r in regex_list:
         result = re.sub(r, '', title)

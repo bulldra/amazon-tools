cdc02e347de4aaa247da90e22a53be442687b520
bulldra <bullda@gmail.com>
2021-05-15 10:14:55 +0900

    正規表現改善

--- src/kindle_util.py
+++ src/kindle_util.py
@@ -3,12 +3,10 @@ __version__ = "0.1.0"
 
 import re
 
-def tlanslate_series_title(title):
-    title = all_remove(title)
-    title = choice_remove(title)
-    return title
+def norm_autohr(atutor):
+    return re.sub(r'\s+','',atutor)
 
-def all_remove(title):
+def tlanslate_series_title(title):
     bracket_word_list =[
         r'雑誌',
         r'部分販売',
@@ -22,7 +20,7 @@ def all_remove(title):
         r'完全版',
         r'書き下ろしイラスト付',
         r'図解',
-        r'.*描き下ろし特典付',
+        r'描き下ろし特典付',
         r'完',
         r'ファンタジア文庫',
         r'フルカラー',
@@ -39,7 +37,11 @@ def all_remove(title):
         r'分割版',
         r'新訳版',
         r'ワイド版',
+        r'イラスト特典付',
+        r'電子版限定特典付き',
+        r'単話版'
     ]
+
     space_word_list = [
         r'通常版',
         r'完全版',
@@ -47,33 +49,27 @@ def all_remove(title):
         r'モノクロ版',
     ]
 
-    regex_list =[r'\s*[\[［〔【\(（]' + x + r'[】〕\]\)）］]\s*' for x in bracket_word_list]
-    regex_list += [r'\s*' + x + r'\s*' for x in space_word_list]
-
-    for r in regex_list:
-        title = re.sub(r,'',title, flags=re.IGNORECASE)
-    return title
-
-def choice_remove(title):
     keyword = r'\d一二三四五六七八九十IⅤＩXⅠⅡ上中下全'
 
-    regex_list =[
-        (r'([\s\d]+年)*\s*[\d]+月号\s*',''),
-        (r'\s*[VＶ][OＯ][LＬ．][\.．]\s*[\d]+\s*',''),
-        (r'\s*[ＮN][OＯ][\.．]\s*[\d]+\s*',''),
-        (r'\s*―*[第全]*[' + keyword + ']+巻―*\s*',''),
-        (r'\s*\([' + keyword + r']\)\s*',''),
-        (r'\s*（[' + keyword + r']+）\s*',''),
-        (r'\s*＜[' + keyword + r']+＞\s*',''),
-        (r'[：\s]+[' + keyword + r']+\s+',''),
+    regex_list =[(r'\s*[\[［〔【\(（]' + x + r'[】〕\]\)）］]\s*',) for x in bracket_word_list]
+    regex_list += [(r'\s*' + x + r'\s*',) for x in space_word_list]
+    regex_list +=[
+        (r'([\s\d]+年)*\s*[\d]+月号\s*',),
+        (r'\s*[VＶ][OＯ][LＬ．][\.．]\s*[\d]+\s*',),
+        (r'\s*[ＮN][OＯ][\.．]\s*[\d]+\s*',),
+        (r'\s*―*[第全]*[' + keyword + ']+巻―*\s*',),
+        (r'\s*\([' + keyword + r']\)\s*',),
+        (r'\s*（[' + keyword + r']+）\s*',),
+        (r'\s*＜[' + keyword + r']+＞\s*',),
+        (r'[：:\s]+[' + keyword + r']+\s+',),
         (r'([^' + keyword + r'+])[' + keyword + r']+\s\(','\\1('),
-        (r'[：\s]*[' + keyword + ']+$',''),
+        (r'([^' + keyword + r'+])[：:\s]*[' + keyword + r']+$','\\1'),
+        (r'[：:\s]*[' + keyword + r']+\(','('),
         (r'\s+\(','('),
     ]
 
     for r in regex_list:
-        result = re.sub(r[0], r[1], title, flags=re.IGNORECASE)
-        if title != result:
-            return result
+        s = r[1] if len(r) == 2 else ''
+        title = re.sub(r[0], s, title, flags=re.IGNORECASE)
 
     return title

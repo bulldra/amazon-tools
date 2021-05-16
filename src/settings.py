b98eb04909b77f3131c344496d487b79da61b8e9
bulldra <bullda@gmail.com>
2021-05-16 17:42:25 +0900

    settings修正取り込み

--- src/settings.py
+++ src/settings.py
@@ -4,28 +4,28 @@ __version__ = "0.1.0"
 import os
 import json
 
-def load_json_setting(path):
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    if os.path.exists(path):
-        with open(path, 'r') as conf:
-            return json.load(conf)
-    else:
-        return None
+def load_json(path):
+    with open(_build_path(path), 'r') as conf:
+        return json.load(conf)
 
-def load_text_list(path):
-    result = []
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    if os.path.exists(path):
-        with open(path, 'r') as conf:
-            for line in conf.read().splitlines():
-                result.append(line)
-            return result
-    else:
-        return None
+def load_text(path):
+    with open(_build_path(path), 'r') as conf:
+        result = []
+        for line in conf.read().splitlines():
+            result.append(line)
+        return result
 
-settings_dict = load_json_setting('../config/settings.json')
-secrets_dict = load_json_setting('../config/secrets.json')
-model_dict = load_json_setting('../config/model.json')
+def _build_path(path):
+    abspath = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
+    abspath = os.path.abspath(abspath)
+    if os.path.exists(abspath):
+        return abspath
+    else:
+        raise FileNotFoundError(abspath)
+
+settings_dict = load_json('../config/settings.json')
+secrets_dict = load_json('../config/secrets.json')
+model_dict = load_json('../config/model.json')
 
 amazon_access_key = secrets_dict['amazon_access_key']
 amazon_secret_key = secrets_dict['amazon_secret_key']

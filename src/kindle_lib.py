69d06dedf13846e008e1ed99de3b71caa134bbe5
bulldra <bullda@gmail.com>
2021-05-16 18:16:35 +0900

    kidle_lib 引数対応

--- src/kindle_lib.py
+++ src/kindle_lib.py
@@ -7,6 +7,7 @@ import settings
 import xml.etree.ElementTree as etree
 import pandas
 import kindle_util
+import sys
 
 class KindleLibExporter:
     def __init__(self):
@@ -41,16 +42,21 @@ class KindleLibExporter:
 
     def main(self, args):
         self.logger.info(args)
-        self.logger.info(f'import {settings.kindle_xml}')
 
-        ## コマンド引数で指定可能にする
-        df = pandas.DataFrame(self.import_kindle_xml(settings.kindle_xml))
+        ## 指定されたファイルパスのXMLを解析
+        self.logger.info(f'import {args.arg1}')
+        df = pandas.DataFrame(self.import_kindle_xml(args.arg1))
         df.sort_values(['series_title','title'], inplace=True)
-        self.logger.info(f'export {settings.kindle_lib}')
-        df.to_csv(settings.kindle_lib, index=False, encoding='UTF-8', sep='\t')
+
+        ## ファイルパスが指定されていればファイルに出力、そうでないなら sys.out に出力
+        out = sys.stdout if args.out is None else args.out
+        self.logger.info(f'export {out}')
+        df.to_csv(out, index=False, encoding='UTF-8', sep='\t')
 
 if(__name__ == '__main__'):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--version', action='version', version=f'version {__version__})')
+    parser.add_argument('--version', action='version', version=f'version {__version__}')
+    parser.add_argument('arg1', help='import file', default=settings.kindle_xml)
+    parser.add_argument('-o','--out', help='output file')
     args = parser.parse_args()
     KindleLibExporter().main(args)
